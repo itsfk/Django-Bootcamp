0cc7d920f0c6192126f07513968650b72c4f266c
Faiz <faiz.ggmu@gmail.com>
2019-01-17 00:46:52 +0500

    Admin Interface For Database

--- firstProject/helloWorldApp/admin.py
+++ firstProject/helloWorldApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from helloWorldApp.models import AccessRecord,Webpage,Topic
 # Register your models here.
+admin.site.register(AccessRecord)
+admin.site.register(Webpage)
+admin.site.register(Topic)
